fe23077480c1a3f370116fa1f8ca47fca7f35bf7
Pavan Kumar <pavankkarkera480@gmail.com>
2025-03-30 20:45:24 +0530

    project cards ui improvement.

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,66 +1,69 @@
 // import React from 'react';
 
-import './ProjectsSection.scss'
+import "./ProjectsSection.scss";
 
-type Props = {}
+type Props = {};
 
 export default function ProjectsSection({}: Props) {
-    const projects = [
-        {
-          title: "Swag of India",
-          description:
-            "Swag of India E-commerce website Project using HTML5, CSS3, Bootstrap4, JavaScript and jQuery",
-          image: "/ecommerce-app.jpg", // Replace with your image path
-          link: "https://pavan-kumar2.github.io/Swag_of_India/",
-        },
-        {
-            title: "Movie-Application",
-            description:
-              "The movie app is a web application that allows users to browse and search for movies, view movie details, and view their IMDb ratings. The app is built using HTML, CSS, JavaScript and React JS for the frontend, and uses a third-party API to retrieve movie data.",
-            image: "/portfolio.jpg", // Replace with your image path
-            link: "https://pavan-kumar2.github.io/Movie-Application/",
-          },{
-            title: "multinational-news-app",
-            description:
-              "Developed a comprehensive, mobile-responsive news browsing application using Angular and RxJS. This project empowers users to explore news articles by country and source, with efficient data flow.",
-            image: "/portfolio.jpg", // Replace with your image path
-            link: "https://multinational-news-app.netlify.app/",
-          },{
-            title: "E commerce app",
-            description:
-              "Developed a comprehensive, mobile-responsive news browsing application using Angular and RxJS. This project empowers users to explore news articles by country and source, with efficient data flow.",
-            image: "/portfolio.jpg", // Replace with your image path
-            link: "https://multinational-news-app.netlify.app/",
-          },{
-            title: "Pavan Kumar",
-            description:
-              "Pavan kunar portfoilio website",
-            image: "/portfolio.jpg", // Replace with your image path
-            link: "",
-          }
-      ];
+  const projects = [
+    {
+      title: "Swag of India",
+      description:
+        "Swag of India E-commerce website Project using HTML5, CSS3, Bootstrap4, JavaScript and jQuery",
+      image: "/ecommerce-app.jpg", // Replace with your image path
+      link: "https://pavan-kumar2.github.io/Swag_of_India/",
+    },
+    {
+      title: "Movie-Application",
+      description:
+        "The movie app is a web application that allows users to browse and search for movies, view movie details, and view their IMDb ratings. The app is built using HTML, CSS, JavaScript and React JS for the frontend, and uses a third-party API to retrieve movie data.",
+      image: "/portfolio.jpg", // Replace with your image path
+      link: "https://pavan-kumar2.github.io/Movie-Application/",
+    },
+    {
+      title: "multinational-news-app",
+      description:
+        "Developed a comprehensive, mobile-responsive news browsing application using Angular and RxJS. This project empowers users to explore news articles by country and source, with efficient data flow.",
+      image: "/portfolio.jpg", // Replace with your image path
+      link: "https://multinational-news-app.netlify.app/",
+    },
+    {
+      title: "E commerce app",
+      description:
+        "Developed a comprehensive, mobile-responsive news browsing application using Angular and RxJS. This project empowers users to explore news articles by country and source, with efficient data flow.",
+      image: "/portfolio.jpg", // Replace with your image path
+      link: "https://multinational-news-app.netlify.app/",
+    },
+    {
+      title: "Pavan Kumar",
+      description: "Pavan kunar portfoilio website",
+      image: "/portfolio.jpg", // Replace with your image path
+      link: "",
+    },
+  ];
 
   return (
     <section className="projects-section">
-        <div className='title'>
-          My Projects For skill develop
-        </div>
+      <div className="title">My Projects For skill develop</div>
 
-          <div className="cards-wrapper">
-          {projects.map((project, index) => (
-            <div className='card' key={index}>
+      <div className="cards-wrapper">
+        {projects.map((project, index) => (
+          <div className="card" key={index}>
+            <div className="wrapper">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
               <img src={project.image} alt={project.title} />
+            </div>
+
+            <div className="character">
+              <img src="project-view.png" />
               <a href={project.link} target="_blank" rel="noopener noreferrer">
                 View Project
               </a>
             </div>
-          ))}
           </div>
-         
-        
-      
-      </section>
-  )
+        ))}
+      </div>
+    </section>
+  );
 }
