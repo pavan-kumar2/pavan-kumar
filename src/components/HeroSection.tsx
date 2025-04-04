3d3d1158d60d1312e4868f2f8d863f054980dc03
Pavan Kumar <pavankkarkera480@gmail.com>
2025-04-04 18:34:03 +0530

    hero section ui improvement.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ type Props = {};
 export default function HeroSection({}: Props) {
   return (
     <section className="hero-section">
-      <div className="left-section">  
+      <div className="left-section">
         <div className="content-container">
           <h2>Hi There! 👋🏻</h2>
           <h2>I'M PAVAN KUMAR</h2>
@@ -15,12 +15,31 @@ export default function HeroSection({}: Props) {
             Fuelled by aggression, driven by persistence, committed to
             continuous learning, and empowered by unyielding determination
           </p>
-          <div className="social-media">
-            <a href=""><img src="LinkedIn.png" alt="LinkedIn"/></a>
-            <a href=""><img src="GitHub.png" alt="GitHub" /></a>
-            <a href=""><img src="X.png" alt="X" /></a>
-          </div>
           <button className="download-btn">get my CV</button>
+          <div className="contact-info">
+            <div className="mail">
+              <span className="label">Email:</span>
+              <a href="mailto:pavankkarkera480@gmail.com">
+                <img className="icon" src="email-icon.svg" />
+                pavankkarkera480@gmail.com
+              </a>
+            </div>
+
+            <div className="social-media-container">
+              <span className="label">Connect with me:</span>
+              <div className="social-media">
+                <a href="">
+                  <img src="LinkedIn.png" alt="LinkedIn" />
+                </a>
+                <a href="">
+                  <img src="GitHub.png" alt="GitHub" />
+                </a>
+                <a href="">
+                  <img src="X.png" alt="X" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="right-section">
